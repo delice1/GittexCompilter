3e0b1ee99746e6fbeec435703db24ee561be0c70
Daniel Elice <delice1@students.towson.edu>
2017-10-23 23:00:13 -0400

    Added rest of constants

--- src/main/scala/edu/towson/cis/cosc455/delice/project1/CONSTANTS.scala
+++ src/main/scala/edu/towson/cis/cosc455/delice/project1/CONSTANTS.scala
@@ -5,5 +5,19 @@ object CONSTANTS {
   val DOCE : String = 	"\\END"
   val TITLEB : String = "\\TITLE["
   val BRACKETE : String = "]"
-
+  val HEADING : String = "#"
+  val PARAB : String = "\\PARAB"
+  val PARAE : String = "\\PARAE"
+  val BOLD : String = "*"
+  val LISTITEM : String = "+"
+  val NEWLINE : String = "\\"
+  val LINKB : String = "["
+  val ADDRESSB : String = "("
+  val ADDRESSE : String = ")"
+  val IMAGEB :String = "!["
+  val DEFB : String = "\\DEF["
+  val EQSIGN : String = "="
+  val USEB : String = "\\USE["
+  //val REQTEXT : String = ("A" || "B" || "C") //or operators are red... how to do all required text?
+  //add text
 }
