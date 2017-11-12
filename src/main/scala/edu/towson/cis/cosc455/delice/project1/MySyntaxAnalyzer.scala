8ee73d89403216486f8eed324b01212cb75a31ab
Daniel Elice <delice1@students.towson.edu>
2017-11-11 22:20:12 -0500

    updates

--- src/main/scala/edu/towson/cis/cosc455/delice/project1/MySyntaxAnalyzer.scala
+++ src/main/scala/edu/towson/cis/cosc455/delice/project1/MySyntaxAnalyzer.scala
@@ -2,9 +2,9 @@ package edu.towson.cis.cosc455.delice.project1
 
 import java.lang
 
-class MySyntaxAnalyzer extends SyntaxAnalyzer {
-
-  val TITLES : List[String] = List(CONSTANTS.TITLEB, CONSTANTS.BRACKETE) //CONSTANTS.VALIDTEXT
+class MySyntaxAnalyzer extends SyntaxAnalyzer
+{
+  var parseTree = scala.collection.mutable.Stack[String]()
 
   var errorFound : Boolean = false
   def setError() = errorFound = true
@@ -12,74 +12,111 @@ class MySyntaxAnalyzer extends SyntaxAnalyzer {
   def getError : Boolean = errorFound
 
   override def gittex(): Unit = {
-    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)){
-      // add to parse tree / stack
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)) {
+      parseTree.push(Compiler.currentToken)
       Compiler.Scanner.getNextToken()
       if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DEFB)){
         variableDefine()
       }
-      else if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.TITLEB)) {
+      else if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.TITLEB)){
         title()
       }
       body()
-      if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCE))
-        {
-          //add to parse tree / stack
-          Compiler.Scanner.getNextToken()
-        }
-      else{
-        println("Error")
-        System.exit(1)
+      if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCE)) {
+        parseTree.push(Compiler.currentToken)
+        Compiler.isEnd = true
+      }
+      else {
+        println("Syntax error, looking for DOCE " + Compiler.currentToken)
+        //System.exit(1)
       }
     }
     else {
-      println("Error")
-      System.exit(1)
+      println("Syntax error, DOCB " + Compiler.currentToken )
+      //System.exit(1)
     }
   }
 
-  override def paragraph(): Unit = ???
+  override def paragraph(): Unit = {
 
-  override def innerItem(): Unit = ???
-
-  override def innerText(): Unit = ???
-
-  override def link(): Unit = ???
-
-  override def italics(): Unit = ???
-
-  override def body(): Unit = ???
-
-  override def bold(): Unit = ???
-
-  override def newline(): Unit = ???
-  /*
-  {
-    if ((NEWLINE contains Compiler.currentToken) || NEWLINE.isEmpty) //right idea, need different syntax
-      Compiler.Scanner.getNextToken()
-    else
-      println("SYNTAX ERROR - A new line was expected h")
   }
-  */
+
+  override def innerItem(): Unit = {
+
+  }
+
+  override def innerText(): Unit = {
+
+  }
+
+  override def link(): Unit = {
+
+  }
+
+  override def body(): Unit = {
+
+  }
+
+  override def bold(): Unit = {
+
+  }
+
+  override def newline(): Unit = {
+    /*
+    {
+      if ((NEWLINE contains Compiler.currentToken) || NEWLINE.isEmpty) //right idea, need different syntax
+        Compiler.Scanner.getNextToken()
+      else
+        println("SYNTAX ERROR - A new line was expected h")
+    }
+    */
+  }
 
   override def title(): Unit = {
-    if(TITLES contains Compiler.currentToken)
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.TITLEB)) {
+      parseTree.push(Compiler.currentToken)
       Compiler.Scanner.getNextToken()
+      //REQTEXT ... need to add
+      //BRACKETE ... need to add
+    }
     else {
-      println("SYNTAX ERROR - A title was expected when '" + Compiler.currentToken + "' was found.")
-      setError()
+      println("SYNTAX ERROR - A title was expected when '" + Compiler.currentToken + "' was found")
+      //System.exit(1)
     }
   }
 
-  override def variableDefine(): Unit = ???
+  override def variableDefine(): Unit = {
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DEFB)) {
+      parseTree.push(Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
+      //REQTEXT
+      //EQUAL SIGN
+    }
+    else {
+      println("Syntax error , defb in var define " + Compiler.currentToken)
+      //System.exit(1)
+    }
+    //REQTEXT
+    //BRACKETE
+    variableDefine()
+  }
 
-  override def image(): Unit = ???
+  override def image(): Unit = {
 
-  override def variableUse(): Unit = ???
+  }
 
-  override def heading(): Unit = ???
+  override def variableUse(): Unit = {
 
-  override def listItem(): Unit = ???
+  }
+
+  override def heading(): Unit = {
+
+  }
+
+  override def listItem(): Unit = {
+
+  }
 
   //def isEmpty() : Boolean //added myself.. who knows!
+
 }
