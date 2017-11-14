adff54dae1d778c5b57b6cc165090564e09df544
Daniel Elice <delice1@students.towson.edu>
2017-11-13 20:12:30 -0500

    DONE.

--- src/main/scala/edu/towson/cis/cosc455/delice/project1/MySemanticAnalyzer.scala
+++ src/main/scala/edu/towson/cis/cosc455/delice/project1/MySemanticAnalyzer.scala
@@ -2,19 +2,33 @@ package edu.towson.cis.cosc455.delice.project1
 
 import java.awt.Desktop
 import java.io.{File, IOException, PrintWriter}
+import edu.towson.cis.cosc455.delice.project1.Compiler.Parser
 
 class MySemanticAnalyzer
 {
   var htmltree = scala.collection.mutable.Stack[String]()
+  var htmltreestring : String = ""
+  var file_name = "project1code"
   //htmltree = Compiler.Parser.parseTree
+
+  def getParseTreeHead() : String = {
+    Parser.parseTree.head
+  }
 
   def startSemantic () = {
     println("START SEMANTIC")
     //htmltree = htmltree.reverse
     println(Compiler.Parser.parseTree)
     htmlcode()
-    println(htmltree.reverse)
-    println(Compiler.Parser.parseTree)
+
+    //println(Compiler.Parser.parseTree)
+    //println("string " + htmltree.reverse.toString()) //converts htmltree reversed to a string
+    htmltreestring = htmltree.reverse.toString()
+    htmltreestring = htmltreestring.replace("Stack(","")
+    htmltreestring = htmltreestring.replace(",","")
+    htmltreestring = htmltreestring.replace("</html>)","</html>")
+    openHTMLFileInBrowser(file_name)
+    //openHTMLFileInBrowser(htmltreestring)
   }
 
   //DOCUMENT
@@ -47,24 +61,27 @@ class MySemanticAnalyzer
   //![text](address) = <img src="ADDRESS" alt="TEXT">
 
   private def htmlcode(): Unit ={
-    println("made it")
+    println("htmlcode beginning")
     println((Compiler.Parser.parseTree.head))
-    if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.DOCB)){
+    println("REVERSED HTML TREE " )
+    println(htmltree.reverse)
+    
+    if (getParseTreeHead().equalsIgnoreCase(CONSTANTS.DOCB)){
       htmltree.push("<html>")
-      Compiler.Parser.parseTree.pop() //needs to keep moving to next things in stack? right logic?
+      Compiler.Parser.parseTree.pop()
       htmlcode()
     }
-    if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.TITLEB)){
+    else if (getParseTreeHead().equalsIgnoreCase(CONSTANTS.TITLEB)){
       htmltree.push("<head>").push("<title>")
-      Compiler.Parser.parseTree.pop()//need to move to next position on stack (POP??)
+      Compiler.Parser.parseTree.pop() //\TITLE[
       htmltree.push(Compiler.Parser.parseTree.head)
-      Compiler.Parser.parseTree.pop()
-      htmltree.push(Compiler.Parser.parseTree.head)
+      Compiler.Parser.parseTree.pop() //The Simpsons
+      //htmltree.push(Compiler.Parser.parseTree.head)
       Compiler.Parser.parseTree.pop()
       htmltree.push("</title").push("<head>")
       htmlcode()
     }
-    if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.HEADING)){
+    else if (getParseTreeHead().equalsIgnoreCase(CONSTANTS.HEADING)){
       htmltree.push("<h1>")
       Compiler.Parser.parseTree.pop()
       htmltree.push(Compiler.Parser.parseTree.head)
@@ -72,31 +89,75 @@ class MySemanticAnalyzer
       htmltree.push("</h1>")
       htmlcode()
     }
-    if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.PARAB)){
+    else if (getParseTreeHead().equalsIgnoreCase(CONSTANTS.PARAB)){
       htmltree.push("<p>")
       Compiler.Parser.parseTree.pop()
       htmlcode()
     }
-    //println(Compiler.Parser.parseTree.head)
-    if (Compiler.Parser.parseTree.head.isText){
+    else if (getParseTreeHead().equalsIgnoreCase(CONSTANTS.LINKB)){
+      htmltree.push("<a href=\" ")
+      Compiler.Parser.parseTree.pop()
+      htmltree.push(Compiler.Parser.parseTree.tail.tail.tail.head)
+      htmltree.push("\">")
       htmltree.push(Compiler.Parser.parseTree.head)
+      htmltree.push("</a>")
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
       Compiler.Parser.parseTree.pop()
       htmlcode()
     }
-    if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.LINKB)){
-      htmltree.push("<a href=\" ")
+    else if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.PARAE)){
+      htmltree.push("</p")
       Compiler.Parser.parseTree.pop()
-      //keep going...
+      htmlcode()
+    }
+    else if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.LISTITEMB)){
+      htmltree.push("<li>")
+      Compiler.Parser.parseTree.pop() //pops +
+      htmltree.push(Compiler.Parser.parseTree.head)
+      Compiler.Parser.parseTree.pop() //pops fname lname
+      htmltree.push("</li>")
+      htmlcode()
+    }
+    else if (Compiler.Parser.parseTree.head.equalsIgnoreCase(CONSTANTS.NEWLINE)){
+      htmltree.push("<br>")
+      Compiler.Parser.parseTree.pop() //pop \\
+      htmlcode()
+    }
+    else if (Compiler.Parser.parseTree.head.equals(CONSTANTS.IMAGEB)){
+      htmltree.push("<img src=")
+      Compiler.Parser.parseTree.pop() //pop ![
+      htmltree.push(Compiler.Parser.parseTree.tail.tail.tail.head)
+      htmltree.push("\"alt=")
+      htmltree.push(Compiler.Parser.parseTree.head)
+      htmltree.push("\"")
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      Compiler.Parser.parseTree.pop()
+      htmlcode()
+    }
+    else if (Compiler.Parser.parseTree.head.equals(CONSTANTS.DOCE)){
+      htmltree.push("</html>")
+      Compiler.Parser.parseTree.pop()
+    }
+    else { //(Compiler.Parser.parseTree.head.isText){
+      htmltree.push(Compiler.Parser.parseTree.head)
+      Compiler.Parser.parseTree.pop()
+      htmlcode()
     }
   }
 
   /* * Hack Scala/Java function to take a String filename and open in default web browswer. */
   def openHTMLFileInBrowser(htmlFileStr : String) = {
+    //println("HTML FS " + htmlFileStr) //htmlFileStr = project1code
     val file : File = new File(htmlFileStr.trim)
     println(file.getAbsolutePath)
     if (!file.exists())
       sys.error("File " + htmlFileStr + " does not exist.")
-
     try {
       Desktop.getDesktop.browse(file.toURI)
     }
@@ -105,5 +166,4 @@ class MySemanticAnalyzer
       case e: Exception => sys.error("He's dead, Jim!")
     }
   }
-
 }
